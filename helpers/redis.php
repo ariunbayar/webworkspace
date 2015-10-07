7fd597f4e3fcc6d55e6cab5958346a12beffbad4
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2015-10-07 10:47:01 +0800

    Fallback as disk for redis

--- helpers/redis.php
+++ helpers/redis.php
@@ -1,14 +1,41 @@
 <?php
-$redis = new Redis();
-$redis->connect('127.0.0.1', 6379);
-$redis->select(2);
+// check if redis is installed
+if (class_exists('Redis')) {
 
-function get($key) {
-    global $redis;
-    return $redis->get($key);
-}
+    $redis = new Redis();
+    $redis->connect('127.0.0.1', 6379);
+    $redis->select(2);
 
-function set($key, $value) {
-    global $redis;
-    return $redis->set($key, $value);
+    function get($key) {
+        global $redis;
+        return $redis->get($key);
+    }
+
+    function set($key, $value) {
+        global $redis;
+        return $redis->set($key, $value);
+    }
+
+} else {
+
+    // fallback method of saving to disk
+    $file = __DIR__ . '/dummy.db';
+    if (!file_exists($file)) {
+        touch($file);
+    }
+
+    function get($key) {
+        global $file;
+
+        $data = unserialize(file_get_contents($file));
+        return $data[$key];
+    }
+
+    function set($key, $value) {
+        global $file;
+        $data = unserialize(file_get_contents($file));
+        $data[$key] = $value;
+        file_put_contents($file, serialize($data));
+    }
+
 }
