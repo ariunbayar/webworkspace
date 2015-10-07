e4647187edbb140458b16d371cd57e7dfaf25b64
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2015-10-07 20:48:02 +0800

    Mouse drag move is not needed anymore

--- web/assets/main.js
+++ web/assets/main.js
@@ -292,60 +292,6 @@ $(function(){
         keyPressed(e.ctrlKey & 1, e.altKey & 1, e.shiftKey & 1, e.which);
     });
 
-    $.fn.drags = function(opt) {
-
-        opt = $.extend({handle:"",cursor:"move",dragstop: null}, opt);
-
-        if(opt.handle === "") {
-            var $el = this;
-        } else {
-            var $el = this.find(opt.handle);
-        }
-
-        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
-            if(opt.handle === "") {
-                var $drag = $(this).addClass('draggable');
-            } else {
-                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
-            }
-            var z_idx = $drag.css('z-index'),
-                drg_h = $drag.outerHeight(),
-                drg_w = $drag.outerWidth(),
-                pos_y = $drag.offset().top + drg_h - e.pageY,
-                pos_x = $drag.offset().left + drg_w - e.pageX;
-            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
-                $('.draggable').offset({
-                    top:e.pageY + pos_y - drg_h,
-                    left:e.pageX + pos_x - drg_w
-                }).on("mouseup", function() {
-                    $(this).removeClass('draggable').css('z-index', z_idx);
-                });
-            });
-            e.preventDefault(); // disable selection
-        }).on("mouseup", function() {
-
-            if (opt.dragstop) {
-                opt.dragstop($(this));
-            }
-            if(opt.handle === "") {
-                $(this).removeClass('draggable');
-            } else {
-                $(this).removeClass('active-handle').parent().removeClass('draggable');
-            }
-        });
-
-    }
-
-    $('.browser').drags();
-
-    $('.watch').drags({
-        dragstop: function(handle){
-            var watch = $(handle).parents('.watch');
-            updateInput(watch);
-        },
-        handle: '.filename'
-    });
-
     $('.watch pre').mousemove(function (e) {
         var pre = $(e.target);
         var tmpBox = $('.tmpBox');
