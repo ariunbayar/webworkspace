479c81c809732a3004ea2789df3ad184810776b0
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2015-12-04 10:20:54 +0800

    Adds a sample logo to give ideas. Can buy on envato

--- templates/layout.php
+++ templates/layout.php
@@ -2,8 +2,11 @@
 <html>
 <head>
     <link rel="stylesheet" href="/assets/main.css" type="text/css" media="all" />
+    <link rel="icon" type="image/png" href="/assets/favicon.png">
 </head>
 <body>
+
+    <img src="/assets/logo.png" style="position: absolute; top: 750px; left: 800px; z-index: 100; box-shadow: 0 0 0 5px #fff, 0 0 0 6px #ccc"/>
 
     <?php echo $mainContent ?>
     <script src="/assets/libs/jquery-2.1.4.min.js"></script>
