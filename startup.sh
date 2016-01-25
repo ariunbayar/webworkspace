779106a664733e4db51a32b3964b4d339e1ceac8
Ariunbayar Baterdene <1480970+ariunbayar@users.noreply.github.com>
2016-01-25 14:33:17 +0800

    Workspace startup file

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dir='/var/www/html/webworkspace'
+
+# start php server
+gnome-terminal -t 'PHP Web Server' --working-directory "$dir/web" -e "php -S 127.0.0.1:8081 index.php"
+
+# load a shell
+gnome-terminal -t 'Webworkspace shell' --working-directory "$dir"
+
+# Load index page
+google-chrome
+sleep 1
+google-chrome "http://localhost:8081"
+google-chrome "https://bitbucket.org/ariunbayar/webworkspace/issues?status=new&status=open"
